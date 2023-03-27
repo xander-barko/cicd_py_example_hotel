c86097915171927c0f040aa02d7faddcf5a63d97
Horváth Andrea <85632079+horandi@users.noreply.github.com>
2023-03-27 08:49:28 +0200

    Update test_hootel.py

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -11,6 +11,9 @@ class TestHotel(object):
         service = Service(executable_path=ChromeDriverManager().install())
         options = Options()
         options.add_experimental_option("detach", True)
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
         self.browser = webdriver.Chrome(service=service, options=options)
         URL = "http://hotel-v2.progmasters.hu/"
         self.browser.get(URL)
